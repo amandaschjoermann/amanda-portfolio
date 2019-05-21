7d5b5acaa24587fcbf07a193ad64ea85ee2cc52b
Amanda Schjørmann <amandaschjoermann@gmail.com>
2019-05-21 15:30:35 +0200

    updated config file

--- config.rb
+++ config.rb
@@ -5,6 +5,15 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
